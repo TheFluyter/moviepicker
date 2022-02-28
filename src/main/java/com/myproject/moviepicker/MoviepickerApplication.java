5a732f8e482975ddfa636bbab992e36b8eabbcd5
Rick de Ruiter <rickderuiter@Ricks-MacBook-Pro.local>
2022-02-28 20:32:12 +0100

    Initial commit

---
+++ src/main/java/com/myproject/moviepicker/MoviepickerApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.moviepicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviepickerApplication.class, args);
+        System.out.println("Test");
+    }
+}
---
+++ src/test/java/com/myproject/moviepicker/MoviepickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.moviepicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviepickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
